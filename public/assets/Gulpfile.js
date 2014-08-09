a80d8fa273b13d5e0097e65c8d0f615b03ac6a28
Niklas Andréasson <andreasson.n@gmail.com>
2014-08-09 09:55:53 +0200

    Add livereload to gulp

--- public/assets/Gulpfile.js
+++ public/assets/Gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass')
+var sass = require('gulp-sass');
+var livereload = require('gulp-livereload');
 
 gulp.task('sass', function () {
     gulp.src('sass/styles.scss')
         .pipe(sass())
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('css'))
+        .pipe(livereload());
 });
 
 gulp.task('watch', function () {
