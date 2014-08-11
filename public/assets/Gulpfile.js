9f7f428865a305e2f51b9f05b6688b4b09a3b5de
Niklas Andréasson <andreasson.n@gmail.com>
2014-08-11 08:45:02 +0200

    Add minify etc to Gulp

--- public/assets/Gulpfile.js
+++ public/assets/Gulpfile.js
@@ -1,12 +1,25 @@
 var gulp = require('gulp');
+
 var sass = require('gulp-sass');
 var livereload = require('gulp-livereload');
+
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 gulp.task('sass', function () {
     gulp.src('sass/styles.scss')
         .pipe(sass())
         .pipe(gulp.dest('css'))
         .pipe(livereload());
+});
+
+gulp.task('scripts', function() {
+    gulp.src('js/**/*.js')
+        .pipe(concat('main.js'))
+        .pipe(rename('main.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('watch', function () {
